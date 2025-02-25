cad17c0fd33897456258dcaf0cde5b6b85c914de
junyeong <gowjr207@gmail.com>
2025-02-25 13:59:50 +0900

    style: 주석 제거

--- src/main/java/kr/wadiz/stripe_demo/DemoService.java
+++ src/main/java/kr/wadiz/stripe_demo/DemoService.java
@@ -20,13 +20,11 @@ public class DemoService {
   public Result savePaymentMethod(SavePaymentMethodRequest savePaymentMethodRequest) throws IllegalAccessException {
     try {
       String paymentMethodId = savePaymentMethodRequest.paymentMethodId();
-      //String customerId = "your_customer_id_here"; 실제로는 고객 ID를 동적으로 받아야 한다.
 
       Customer customer = Customer.create(Map.of(
           "email", "junyeong.jang@wadiz.kr"
       ));
 
-      // 결제 메서드를 고객과 연결
       PaymentMethod paymentMethod = PaymentMethod.retrieve(paymentMethodId);
       PaymentMethodAttachParams attachParams = PaymentMethodAttachParams.builder()
           .setCustomer(customer.getId())
@@ -51,14 +49,11 @@ public class DemoService {
           .setCurrency("usd")
           .setCustomer(customerId)
           .setPaymentMethod(paymentMethodId)
-          .setOffSession(true)  // 고객이 실제로 참석하지 않는 결제
+          .setOffSession(true)
           .setConfirm(true)
           .build();
 
       PaymentIntent paymentIntent = PaymentIntent.create(params);
-
-      // check your json response in Stripe Dashboard. or use this.
-      // return Result.builder().msg(paymentIntent.toJson()).build();
 
       return Result.builder().msg(paymentIntent.getStatus()).build();
     } catch (Exception e) {
